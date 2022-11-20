53168767d54c395e063d71109be876d245426ee3
Mohamed Khaled <deadvan@live.com>
2022-11-20 20:07:11 +0200

    new

--- public/map-overlay/script.js
+++ public/map-overlay/script.js
@@ -1,13 +1,48 @@
 const container = document.querySelector('.container')
 const title = document.querySelector('h1')
+const tableContainer = document.createElement('div')
+tableContainer.classList.add('table-container')
+container.append(tableContainer)
 
-let teams = []
+fetch('/get-teams')
+  .then((response) => response.json())
+  .then((teams) => {
+    teams.forEach((team, index) => {
+      const row = document.createElement('div')
+      row.classList.add(`row-${index + 1}`)
+      row.classList.add('row')
+      tableContainer.append(row)
+      const teamRank = document.createElement('p')
+      teamRank.classList.add(`team-${index + 1}-rank`)
+      teamRank.classList.add('team-rank')
+      row.append(teamRank)
+      const teamLogo = document.createElement('img')
+      teamLogo.classList.add(`team-${index + 1}-logo`)
+      teamLogo.classList.add('team-logo')
+      row.append(teamLogo)
+      const teamName = document.createElement('p')
+      teamName.classList.add(`team-${index + 1}-name`)
+      teamName.classList.add('team-name')
+      row.append(teamName)
+      const teamPts = document.createElement('p')
+      teamPts.classList.add(`team-${index + 1}-pts`)
+      teamPts.classList.add('team-pts')
+      row.append(teamPts)
+      const aliveContainer = document.createElement('div')
+      aliveContainer.classList.add(`alive-container-${index + 1}`)
+      aliveContainer.classList.add('alive-container')
+      row.append(aliveContainer)
+      for (let index = 0; index < 4; index++) {
+        const playerAlive = document.createElement('div')
+        playerAlive.classList.add(`player-${index + 1}-alive`)
+        playerAlive.classList.add('player-${index + 1}-alive')
+        aliveContainer.append(playerAlive)
+      }
+    })
+  })
 
 setInterval(() => {
   fetch('/get-teams')
     .then((response) => response.json())
-    .then((teams) => {
-      console.clear()
-      console.log(teams)
-    })
+    .then((teams) => {})
 }, 1000)
